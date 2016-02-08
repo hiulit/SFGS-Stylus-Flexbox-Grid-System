4228e5a0b127cfeb7eb63f4cb321714425cea116
hiulit <hiulit@gmail.com>
2016-02-08 14:18:38 +0100

    Added default tasks for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,38 @@
-// Gruntfile.js
-module.exports = grunt => {
-    // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
-    require('load-grunt-tasks')(grunt);
+'use strict';
 
-    grunt.initConfig({
-      watch: {
-        stylus: {
-          files: ['stylus/{,*/,**/}*.styl'],
-          tasks: ['stylus:compile'],
-          // options: {
-          //   livereload: true
-          // }
-        },
-        stylus: {
-          compile: {
-            options: {
-              sourcemap: {
-                inline: true
-              },
-              compress: true,
-              use: [
-                function() {
-                  return require('autoprefixer-stylus')('last 2 versions', 'ie 8-10');
-                }
-              ],
-              files: {'css/main.css', 'stylus/main.styl'}
-            }
-          }
-        }
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      stylus: {
+        files: ['stylus/{,*/,**/}*.styl'],
+        tasks: ['stylus:compile']
       }
-    });
+    },
+    stylus: {
+      compile: {
+        options: {
+          sourcemap: {
+            inline: true
+          },
+          compress: true,
+          use: [
+            function() {
+              return require('autoprefixer-stylus')('last 2 versions', 'ie 8-10');
+            }
+          ]
+        },
+        files: {'css/main.css': 'stylus/main.styl'}
+      }
+    }
+  });
 
-    grunt.registerTask('default', [
-      grunt.task.run([
-        'stylus:compile'
-        'watch'
-      ]);
-    ]);
+  grunt.registerTask('default', [
+    'stylus:compile',
+    'watch'
+  ]);
+
 };

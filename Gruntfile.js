29afad83d2cdc962fa3442d3e68924974dcdfb22
hiulit <hiulit@gmail.com>
2016-04-04 11:50:23 +0200

    Added grunt build and fixed autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
           "include css": true,
           use: [
             function() {
-              return require('autoprefixer-stylus')('last 2 versions', 'ie 8-10');
+              return require("autoprefixer-stylus")("last 2 versions", "ie 8-11", "Safari");
             }
           ]
         },
@@ -51,6 +51,11 @@ module.exports = function(grunt) {
     'stylus:compile',
     'postcss',
     'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'stylus:compile',
+    'postcss',
   ]);
 
 };
